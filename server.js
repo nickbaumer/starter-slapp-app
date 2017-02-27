4122507657bc4eb9f45421fb8dfed2af7e8b2125
Nick Baumer <nick.baumer@gmail.com>
2017-02-27 20:39:01 +0000

    another approach to dates

--- server.js
+++ server.js
@@ -116,9 +116,9 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
 // LAN message
 slapp.
   message('\\b(lan)\\b', 'ambient', (msg) => {
-    var today = Date.now();
+    var today = new Date();
     var lan = new Date('13/04/2017 18:00');
-    var difference = (lan.getTime() - today);
+    var difference = (lan.getTime() - today.getTime());
     msg
       .say([
       'LAN starts at 18:00 on Thursday the 13th of April 2017.',

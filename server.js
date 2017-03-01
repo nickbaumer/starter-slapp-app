513f4432501785178aef5594d7fc71e2fe9e02ba
Nick Baumer <nick.baumer@gmail.com>
2017-03-01 15:43:19 +0000

    more thanks

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ slapp.
     ]).route('thanks')
   }).route('thanks', (msg) => {
     var text = (msg.body.event && msg.body.event.text) || ''
-    if (text.includes("thanks")) {
+    if (text.match(/(thanks|cheers|ta|thx|thank you|/i))) {
       return msg
         .say([
           'Douchebag.',

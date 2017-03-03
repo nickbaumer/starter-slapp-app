5d4da09cbeca8ee602ae63a2c3d2f778d6502a0c
Nick Baumer <nick.baumer@gmail.com>
2017-03-03 19:47:15 +0000

    testing config

--- quote.js
+++ quote.js
@@ -1,5 +1,13 @@
 module.exports = function(slapp) {
-  slapp.message('attach', ['direct_message'], (msg) => {
-    msg.say('pong')
+  slapp.message('1 attachments', ['direct_message'], (msg) => {
+    msg.say(msg.body)
+  })
+
+  slapp.message('ping', ['direct_message'], (msg) => {
+    msg.say(msg.body)
+  })
+
+  slapp.message('pong', ['direct_message'], (msg) => {
+    msg.say('ping')
   })
 }

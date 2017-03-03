477c431a970fc30d13067f3bc79c52162f35ec49
Nick Baumer <nick.baumer@gmail.com>
2017-03-03 19:51:42 +0000

    changed handling attachments

--- quote.js
+++ quote.js
@@ -1,10 +1,10 @@
 module.exports = function(slapp) {
-  slapp.message('1 attachments', ['direct_message'], (msg) => {
-    msg.say(msg.body)
+  slapp.message('1 attachments', ['direct_message'], (msg, attachments) => {
+    msg.say(attachments.text)
   })
 
-  slapp.message('ping', ['direct_message'], (msg) => {
-    msg.say(msg.body)
+  slapp.message('ping', ['direct_message'], (msg, text) => {
+    msg.say(text)
   })
 
   slapp.message('pong', ['direct_message'], (msg) => {

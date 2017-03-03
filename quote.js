3f99a134b068c7deb733584adf5a0cfd84b73135
Nick Baumer <nick.baumer@gmail.com>
2017-03-03 20:02:23 +0000

    changed attachment check

--- quote.js
+++ quote.js
@@ -1,8 +1,4 @@
 module.exports = function(slapp) {
-  slapp.message('', ['direct_message'], (msg, attachments) => {
-    console.log('You trigged a catch all.')
-    msg.say(attachments.text)
-  })
 
   slapp.message('ping', ['direct_message'], (msg, text) => {
     msg.say(text)
@@ -11,4 +7,10 @@ module.exports = function(slapp) {
   slapp.message('pong', ['direct_message'], (msg) => {
     msg.say('ping')
   })
+
+  slapp.message('add quote', ['direct_message'], (msg, attachments) => {
+    msg.say(attachments.author_name)
+    msg.say(attachments.text)
+    })
+
 }

8ece888be2a0ac8c987c3747bf6ed66b4078f93b
Nick Baumer <nick.baumer@gmail.com>
2017-03-03 19:13:55 +0000

    tutorial and LAN split into separate files

---
+++ lan.js
@@ -0,0 +1,30 @@
+module.exports = function(slapp) {
+  slapp.
+    message('(\\b(lan)\\b)|(\\b(Ian)\\b)', 'ambient', (msg) => {
+      var today = new Date();
+      var lan = new Date('April 13, 2017 18:00');
+      var difference = (lan - today.getTime()) / 1000;
+      msg
+        .say([
+        'LAN starts at 18:00 on Thursday the 13th of April 2017.',
+        'la la la la I\'m not listening',
+        '6pm. Thursday. 13/04.',
+        'Ask @vyper',
+        difference + ' seconds to go.',
+        Math.round(difference / 60) + ' minutes to go.',
+        Math.round(difference / 60 / 60) + ' hours to go.',
+        Math.round(difference / 86400) + ' days to go.'
+      ]).route('thanks')
+    }).route('thanks', (msg) => {
+      var text = (msg.body.event && msg.body.event.text) || ''
+      if (text.match(/(thanks|cheers|ta|thx|thank you)/i)) {
+        return msg
+          .say([
+            'Douchebag.',
+            'Dickhole.',
+            'Tool.',
+            'Ass bandit.'
+          ])
+    }
+  })
+};

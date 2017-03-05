d92ad270082e9c5335c12f168ec939edfe2f0b7f
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 11:34:36 +0000

    meant to use msg.body

--- quote.js
+++ quote.js
@@ -3,7 +3,7 @@ module.exports = function(slapp) {
   slapp.message('', ['direct_message'], (msg) => {
 //    var type = msg.body.event.attachments.text
     console.log('Attempting to log body')
-    console.log(body)
+    console.log(msg.body)
     })
 
 }

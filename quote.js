8a705c7db152e93a443d0250eb09a5c66d1fb8c1
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 14:32:14 +0000

    changed understanding

--- quote.js
+++ quote.js
@@ -1,24 +1,16 @@
 module.exports = function(slapp) {
 
-  slapp.message('^$', ['direct_message'], (msg, state) => {
+  slapp.message('^$', ['direct_message'], (msg) => {
     var user_name = require('./get_user')(msg)
     //console.log('User identified as:' + user_name)
     //console.log('Checking for attachments')
     var attachments = msg.body.event.attachments[0]
     if (attachments) {
-      //console.log('text:' + attachments.text)
-      //console.log('author_subname:' + attachments.author_subname)
-      //console.log('ts:' + attachments.ts)
-      if (!state.date) {
-        var firstQuote = 'It looks like you\'re trying to add a quote\\n'
-        state.date = attachments.ts
-        state.nick = user_name
-      }
-      state.quote = attachments.author_subname + ': ' + attachments.text + '/n'
-      state.test = 'crescent moon'
+      var strQuote = attachments.author_subname + ': ' + attachments.text + '/n'
       return msg
-      .say(firstQuote + 'Are there more lines?')
-      .route('more-lines', state)
+      .say('It looks like you\'re trying to add a quote.')
+      .say('Are there more lines?')
+      .route('more-lines', {date: attachments.ts, nick: user_name, quote: strQuote})
     }
   })
   .route('more-lines', (msg, state) => {

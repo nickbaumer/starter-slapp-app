2ac0a960b2d4056eb142147140bade364918d725
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 17:55:11 +0000

    testing!

--- get_user.js
+++ get_user.js
@@ -1,27 +1,32 @@
-function GetUser(msg) {
-  var request = require('request')
-  // Set the headers
-  var headers = {
-      'User-Agent':       'Super Agent/0.0.1',
-      'Content-Type':     'application/x-www-form-urlencoded'
-  }
-  // Configure the request
-  var options = {
-      url: 'https://slack.com/api/users.info',
-      method: 'POST',
-      headers: headers,
-      form: {'token': msg.meta.bot_token, 'user': msg.body.event.user}
-  }
-  // Start the request
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-          // Print out the response body
-          var result = JSON.parse(body);
-          var user_name = result.user.name;
-          return user_name;
-      }
-  };
-  return request(options, callback);
+module.exports = function(msg) {
+
+var request = require('request')
+
+// Set the headers
+var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+
+// Configure the request
+var options = {
+    url: 'https://slack.com/api/users.info',
+    method: 'POST',
+    headers: headers,
+    form: {'token': msg.meta.bot_token, 'user': msg.body.event.user}
+}
+
+// Start the request
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        var result = JSON.parse(body);
+        var user_name = result.user.name;
+        return user_name;
+    }
 };
 
-module.exports = GetUser;
+return request(options, callback);
+
+};

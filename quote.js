2c556b2e04303d9e83484bd8887992a5b8b07662
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 12:12:06 +0000

    syntax issue

--- quote.js
+++ quote.js
@@ -3,7 +3,7 @@ module.exports = function(slapp) {
   slapp.message('', ['direct_message'], (msg) => {
     console.log('Attempting to log attachments')
     var attachments = msg.body.event.attachments[0]
-    console.log('text:'+attachments.text)
-    console.log('author_name':+attachments.author_name)
-    console.log('ts:'+attachments.ts)
+    console.log('text:' + attachments.text)
+    console.log('author_name:' + attachments.author_name)
+    console.log('ts:' + attachments.ts)
 }

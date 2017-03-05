318c1c4dca18750fac8007fd87b8710838689472
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 13:48:19 +0000

    various syntax changes!

--- get_user.js
+++ get_user.js
@@ -1,7 +1,7 @@
 module.exports = function(msg) {
 
 var request = require('request')
-
+console.log('user:' + msg.body.event.user)
 // Set the headers
 var headers = {
     'User-Agent':       'Super Agent/0.0.1',
@@ -20,6 +20,9 @@ var options = {
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
+        console.log(body);
+        console.log(body.user)
+        console.log(body.user.name)
         return body.user.name;
     }
 })

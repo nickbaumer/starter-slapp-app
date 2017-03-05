e63af51ffc085574afe06cb69889ef23762ef2e6
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 13:35:37 +0000

    moved location of code

--- quote.js
+++ quote.js
@@ -1,39 +1,14 @@
-var request = require('request')
-
-// Set the headers
-var headers = {
-    'User-Agent':       'Super Agent/0.0.1',
-    'Content-Type':     'application/x-www-form-urlencoded'
-}
-
-
-
-
-
 module.exports = function(slapp) {
 
   slapp.message('^$', ['direct_message'], (msg) => {
-    // Start the request
-    // Configure the request
-    var options = {
-        url: 'https://slack.com/api/users.info',
-        method: 'POST',
-        headers: headers,
-        form: {'token': msg.meta.bot_token, 'user': 'U2FJ2PR2Q'}
-    }
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            console.log(body)
-        }
-    })
+    require('./test_request')(msg)
     console.log('Checking for attachments')
     var attachments = msg.body.event.attachments[0]
     if (attachments) {
       console.log('text:' + attachments.text)
       console.log('author_subname:' + attachments.author_subname)
       console.log('ts:' + attachments.ts)
-      console.log(msg.meta.bot_token)
+      console.log(msg.meta.)
       if (!state || !state.date) {
         state.date = attachments.ts
         state.nick = msg.body.event.user
---
+++ test_request.js
@@ -0,0 +1,26 @@
+module.exports = function(msg) {
+
+var request = require('request')
+
+// Set the headers
+var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+
+// Configure the request
+var options = {
+    url: 'https://slack.com/api/users.info',
+    method: 'POST',
+    headers: headers,
+    form: {'token': msg.meta.bot_token, 'user': 'U2FJ2PR2Q'}
+}
+
+// Start the request
+request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.log(body)
+    }
+})
+};

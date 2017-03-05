db55fdd14811d02fa9a14fd097c3a9a20eb221c3
Nick Baumer <nick.baumer@gmail.com>
2017-03-05 13:38:38 +0000

    small changes

--- quote.js
+++ quote.js
@@ -8,10 +8,10 @@ module.exports = function(slapp) {
       console.log('text:' + attachments.text)
       console.log('author_subname:' + attachments.author_subname)
       console.log('ts:' + attachments.ts)
-      if (!state || !state.date) {
+      //if (!state || !state.date) {
         state.date = attachments.ts
         state.nick = msg.body.event.user
-      }
+      //}
       state.quote = attachments.author_subname + ': ' + attachments.text + '/n'
       return msg
       .say('It looks like you\'re trying to add a quote')

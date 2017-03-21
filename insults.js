c3f350fcde686acb276d8261020deff14e57643a
Nick Baumer <nick.baumer@gmail.com>
2017-03-21 20:05:20 +0000

    change of syntax

--- insults.js
+++ insults.js
@@ -1,32 +1,28 @@
-var insults = {
-  function(slapp) {
-    var request = require('request')
-    var url = 'https://www.foaas.com/thanks/nick'
-    var options = {
-      url: url,
-      method: 'GET'
-    }
-
-    function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            var result = JSON.parse(body)
-            var message = result.message
-            var subtitle = result.subtitle
-            console.log('message:' + message)
-            console.log('subtitle:' + subtitle)
-        }
-    }
-
-    var regex = '.*'
-    var location = 'ambient'
-
-    slapp.
-      message(regex,location, (msg) => {
-        request(options, callback)
-      })
-
+module.exports = function(slapp) {
+  var request = require('request')
+  var url = 'https://www.foaas.com/thanks/nick'
+  var options = {
+    url: url,
+    method: 'GET'
   }
-}
 
-module.exports = insults
+  function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+          // Print out the response body
+          var result = JSON.parse(body)
+          var message = result.message
+          var subtitle = result.subtitle
+          console.log('message:' + message)
+          console.log('subtitle:' + subtitle)
+      }
+  }
+
+  var regex = '.*'
+  var location = 'ambient'
+
+  slapp.
+    message(regex,location, (msg) => {
+      request(options, callback)
+    })
+
+}

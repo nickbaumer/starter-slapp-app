802acc21c1c9be692bd4bbc806ff25042aad05b6
Nick Baumer <nick.baumer@gmail.com>
2017-03-21 19:58:41 +0000

    teach me to node

--- insults.js
+++ insults.js
@@ -1,33 +1,33 @@
-var request = require('request')
-
 var insults = {
-
-  var url = 'https://www.foaas.com/thanks/nick'
-  var options = {
-    url: url,
-    method: 'GET'
-  }
-
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-          // Print out the response body
-          var result = JSON.parse(body)
-          var message = result.message
-          var subtitle = result.subtitle
-          console.log('message:' + message)
-          console.log('subtitle:' + subtitle)
-      }
-  }
-
-  var regex = '.*'
-  var location = 'ambient'
-
   function(slapp) {
-    slapp.
-      message(regex,location, (msg) => {
-        request(options, callback)
-      }
+    var request = require('request')
+    var url = 'https://www.foaas.com/thanks/nick'
+    var options = {
+      url: url,
+      method: 'GET'
+    }
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            var result = JSON.parse(body)
+            var message = result.message
+            var subtitle = result.subtitle
+            console.log('message:' + message)
+            console.log('subtitle:' + subtitle)
+        }
+    }
+
+    var regex = '.*'
+    var location = 'ambient'
+
+    function(slapp) {
+      slapp.
+        message(regex,location, (msg) => {
+          request(options, callback)
+        }
+    }
   }
 }
 
-module.exports = insults(slapp)
+module.exports = insults
